96391ff78b2aa122857f9c024219d5af0bf697a8
danny1015 <airmax1015@gmail.com>
2022-11-04 17:52:34 +0800

    test

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+#dannytest
 terraform {
   backend "gcs" {
     bucket = "idyllic-journey-126508-tfstate"
